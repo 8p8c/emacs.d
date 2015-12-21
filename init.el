945b59dc1464007222d6ab02b32d54287cdac72e
Dmitry Nikolaev <cameltheman@gmail.com>
2015-12-21 23:27:54 +0300

    Add whitespace and smart-tabs.

--- init.el
+++ init.el
@@ -1,5 +1,19 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-(setq indent-tabs-mode t)
-(setq tab-width 4)
+(require 'cl-lib)
+
+(require 'package)
+(package-initialize)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(require 'smart-tabs-mode)
+(smart-tabs-insinuate 'c 'ruby 'java)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(require 'whitespace)
+(setq-default whitespace-style '(face tabs spaces trailing space-before-tab newline empty space-after-tab space-mark tab-mark newline-mark))
+(global-whitespace-mode 1)
